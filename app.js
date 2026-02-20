27a31632f83424cb18df6fb22e1c31c84a756a80
Endri-Tosku <endritosku001@gmail.com>
2026-02-20 16:41:47 +0100

    setup base middleware

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// importo il middelware errori
+const notFound = require("./middlewares/notFound")
+
+// importo il middelware errorif
+const errorsHandler = require("./middlewares/errorsHandler")
+
+// importiamo le rotte dei post
+// const postRoutes = require('./‎routes/post');
+
+// attivazione cartella public per i file statici
+app.use(express.json());
+
+// middleware per leggere JSON (servirà dopo)
+// app.use(express.json());
+
+// rotta di home
+app.get("/", (req, res) => {
+    res.send("<h1>Rotta di home della APP dei film</h1>");
+});
+
+// colleghiamo le rotte dei post
+// app.use('/posts', postRoutes);
+
+// middleware per endpoint non trovati
+app.use(notFound);
+
+// middleware globale per gestione errori
+app.use(errorsHandler);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
